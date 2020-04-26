03680ecb78bd38eee3ef27ecbc10a10990f4a59e
TENCENT\v_awjliu <v_awjliu@tencent.com>
2020-04-26 16:24:06 +0800

    1

--- home_application/models.py
+++ home_application/models.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+
+class Inquire(models.Model):
+    time = models.DateTimeField(verbose_name='时间')
+    key = models.CharField(max_length=128, verbose_name='关键字')
+
+
+
+class Esconfig(models.Model):
+    ip = models.CharField(max_length=128, verbose_name='IP')
+    port = models.CharField(max_length=128, verbose_name='端口')
+    user = models.CharField(max_length=128, verbose_name='用户', default='')
+    password = models.CharField(max_length=128, verbose_name='密码', default='')
+    indexname = models.CharField(max_length=128, verbose_name='索引')
+
--- home_application/views.py
+++ home_application/views.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-
+from blueking.component.shortcuts import get_client_by_request
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
 # 装饰器引入 from blueapps.account.decorators import login_exempt
 def home(request):
-    """
-    首页
-    """
+    client = get_client_by_request(request)
+    res = client.cc.search_business()
+
+
     return render(request, 'home_application/index_home.html')
 
 
